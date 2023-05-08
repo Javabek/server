9b12ff2bdb9c4399c68297de8adb31eb0b0578ef
Javabek <javabek1203@gmail.com>
2023-05-09 03:07:39 +0900

    feat handle the request body

--- index.js
+++ index.js
@@ -5,19 +5,26 @@ const http = require("http");
 
 app.use(express.static("public"));
 app.use(express.json());
-app.use(express.urlencoded({extended : true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.set("views", "views");
 app.set("view engine", "ejs");
 
-app.get("/hello", function (req,res) {
+app.get("/hello", function (req, res) {
   res.end("<h1> Hello World </h1>")
 });
-app.get("/gifts", function (req,res) {
-  res.end("<h1> YOU ARE IN GIFTS Section </h1>")
+
+app.get("/", function (req, res) {
+  res.render("harid")
 });
+
+app.post("/create-item", (req, res) => {
+  console.log(req.body.newItem);
+  res.json({tets: "success"});
+})
+
 const server = http.createServer(app);
 let PORT = 3000;
-server.listen(PORT, function(){
+server.listen(PORT, function () {
   console.log(`The server is running successfully on port ${PORT}`);
 })
